b8624c7f74e0990e696a708d1741a95add87599c
王波 <1716936609@qq.com>
2023-05-20 10:54:36 +0800

    修改

--- Webwangbo519/Webwangbo519/Controllers/ZuoyeController.cs
+++ Webwangbo519/Webwangbo519/Controllers/ZuoyeController.cs
@@ -14,7 +14,9 @@ namespace Webwangbo519.Controllers
             bool list1 = true;//bool
             float flo1 =5;//单精度
             double dou1 = 5.43;//双精度
-            
+            decimal dec1 =67;
+            long lon1= 9223372036854775807;
+
             //装箱
             int int1 = 100;
             object obj1 = int1;
@@ -24,7 +26,18 @@ namespace Webwangbo519.Controllers
             object obj2 = int2;
             int num = (int)obj2;
 
-            return Ok(new{ list, list1, flo1, dou1, obj1, num });
+            return Ok(new{ list, list1, flo1, dou1, dec1, lon1, obj1, num });
         }
+
+    }
+    /// <summary>
+    /// 枚举 枚举类型是由基础整型数值类型的一组命名常量定义的值类型
+    /// </summary>
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Aurumn,
+        Winter
     }
 }
